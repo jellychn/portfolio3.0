992a85402bb1a89bee333763852f05ce54eeb9f7
Jerry Chen <jchen123.862@gmail.com>
2023-12-28 13:46:43 +1300

    remove email

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -2,8 +2,6 @@ import "./Footer.scss";
 
 import logo from "../../assets/logo-inverse.svg";
 import linkedIn from "../../assets/linked-in.svg"
-import email from "../../assets/email.svg";
-
 export default function Footer(): JSX.Element {
     return (
         <div className="footer">
@@ -14,9 +12,6 @@ export default function Footer(): JSX.Element {
                     <div className="socials-container">
                         <a href="https://www.linkedin.com/in/jerry-chen-3235171b4/" target="_blank">
                             <img className="socials" src={ linkedIn } alt="" />
-                        </a>
-                        <a href="mailto:jchen123.862@gmail.com">
-                            <img className="socials" src={ email } alt="" />
                         </a>
                     </div>
                 </div>
